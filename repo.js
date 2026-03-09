8af82aaf3488a06fb9b13b7f307335dd884e6d08
Jan Hořava <czechinthemail@gmail.com>
2026-03-09 02:38:09 -0700

    added force arg usage when core_escape() isn't toggle

--- repo.js
+++ repo.js
@@ -99,7 +99,7 @@ function repo_init(){
         'reset_camera': {
           'onclick': function(){
               reset_camera();
-              core_escape();
+              core_escape(false);
           },
         },
       },
